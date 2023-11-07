5da8384b9239065696f4ed465de4004c24fffe52
SpilnykSofiia2003 <spilniks03@gmail.com>
2023-11-07 14:45:03 +0200

    refactoring code

--- src/test/java/org/example/TestCalculator.java
+++ src/test/java/org/example/TestCalculator.java
@@ -1,13 +1,17 @@
+package org.example;
+
 import org.junit.Test;
 import org.example.Calculator;
-import org.junit.Assert;
+
+
+import static org.junit.Assert.assertEquals;
 
 public class TestCalculator {
     @Test
     public void testAdd() throws Exception{
         Calculator calculator  = new Calculator();
         int add = calculator.add(3,4);
-        Assert.assertEquals(7, add);
+        assertEquals(7, add);
 
     }
 }

478dcb8e59157d3e4d14fdafff7dcb34d6f8f267
SpilnykSofiia2003 <spilniks03@gmail.com>
2023-11-20 20:49:39 +0200

    123

--- src/test/java/org/example/TestCalculator.java
+++ src/test/java/org/example/TestCalculator.java
@@ -11,7 +11,28 @@ public class TestCalculator {
     public void testAdd() throws Exception{
         Calculator calculator  = new Calculator();
         int add = calculator.add(3,4);
-        assertEquals(7, add);
+        assertEquals(8, add);
+
+    }
+    @Test
+    public void testAdd1() throws Exception{
+        Calculator calculator  = new Calculator();
+        int add = calculator.add(5,6);
+        assertEquals(11, add);
+
+    }
+    @Test
+    public void testAdd2() throws Exception{
+        Calculator calculator  = new Calculator();
+        int add = calculator.add(20,11);
+        assertEquals(31, add);
+
+    }
+    @Test
+    public void testAdd3() throws Exception{
+        Calculator calculator  = new Calculator();
+        int add = calculator.add(0,-2);
+        assertEquals(-2, add);
 
     }
 }
